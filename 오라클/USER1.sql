2f0ecc8dcdafb7c450e783ba7d375e0b59e81d54
JtoJ00721 <moonlf2000@naver.com>
2020-12-01 16:44:45 +0900

    2020 마지막 MVC 패턴 실습 시작

---
+++ 오라클/USER1.sql
@@ -0,0 +1,19 @@
+-- 2020년 마지막 MVC 패턴 실습
+
+CREATE TABLE tbl_employees (
+
+id	NUMBER		PRIMARY KEY,
+first_name	nVARCHAR2(30)	NOT NULL	,
+last_name	nVARCHAR2(30)	NOT NULL	,
+address	nVARCHAR2(125)		,
+tel	VARCHAR2(20)		,
+email	VARCHAR2(125)		
+
+);
+
+CREATE SEQUENCE seq_employees
+START WITH 1 INCREMENT BY 1;
+
+SELECT * FROM tbl_employees;
+
+
